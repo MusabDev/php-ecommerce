60c3e83b90ae8c0ca0d8160f7eb54c385ccc17c7
Muhammad Musab <musabwebdev@gmail.com>
2022-06-11 10:53:29 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# E-Commerce HTML Template
+
+This is a simple E-Commerce template built on HTML, CSS, JS and Bootstrap 5.
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,php}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  plugins: [],
+}
